6843beec878cef02749e0d87081bcb71c73b4ebf
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-04-25 20:44:56 +0530

    header complete

--- portfolio/portfolioFrontend/src/components/header/Header.jsx
+++ portfolio/portfolioFrontend/src/components/header/Header.jsx
@@ -1,48 +1,39 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./header.css";
 
 export default function Header() {
+  const linkCss = ({ isActive }) =>
+    isActive
+      ? "text-orange-500 custom-underline-active"
+      : "text-black custom-underline";
+
   return (
-    <header className="bg-white justify-center text-black p-5 flex shadow-md">
+    <header className="fixed top-0 left-0 w-full z-50 bg-white/40 backdrop-blur-sm justify-center text-black p-5 flex shadow-md ">
       <nav>
         <ul className="flex gap-5">
           <li className="text-2xl p-3 font-semibold">
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? "text-blue-600" : "")}
-            >
-              Home
+            <NavLink to="/" className={linkCss}>
+              <h1>Home</h1>
+            </NavLink>
+          </li>
+          <li className="text-2xl p-3 font-semibold ">
+            <NavLink to="/about" className={linkCss}>
+              <h1>About</h1>
             </NavLink>
           </li>
           <li className="text-2xl p-3 font-semibold">
-            <NavLink
-              to="/about"
-              className={({ isActive }) => (isActive ? "text-blue-600" : "")}
-            >
-              About Me
+            <NavLink to="/projects" className={linkCss}>
+              <h1>Projects</h1>
             </NavLink>
           </li>
           <li className="text-2xl p-3 font-semibold">
-            <NavLink
-              to="/projects"
-              className={({ isActive }) => (isActive ? "text-blue-600" : "")}
-            >
-              Projects
-            </NavLink>
-          </li>
-          <li className="text-2xl p-3 font-semibold">
-            <NavLink
-              to="/blogs"
-              className={({ isActive }) => (isActive ? "text-blue-600" : "")}
-            >
+            <NavLink to="/blogs" className={linkCss}>
               Blogs
             </NavLink>
           </li>
           <li className="text-2xl p-3 font-semibold">
-            <NavLink
-              to="/contact"
-              className={({ isActive }) => (isActive ? "text-blue-600" : "")}
-            >
+            <NavLink to="/contact" className={linkCss}>
               Contact Me
             </NavLink>
           </li>
