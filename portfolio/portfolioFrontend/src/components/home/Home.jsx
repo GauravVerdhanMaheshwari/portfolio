86459a6405b6e8cecb44d6d0074da3a8b69ae942
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-04-27 23:05:23 +0530

    made header

--- portfolio/portfolioFrontend/src/components/home/Home.jsx
+++ portfolio/portfolioFrontend/src/components/home/Home.jsx
@@ -3,54 +3,22 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="mx-auto w-full max-w-7xl">
-      <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
-        <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
-          <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
-            <h2 className="text-4xl font-bold sm:text-5xl">
-              Download Now
-              <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
-            </h2>
-
-            <Link
-              className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
-              to="/"
-            >
-              <svg
-                fill="white"
-                width="24"
-                height="24"
-                xmlns="http://www.w3.org/2000/svg"
-                fillRule="evenodd"
-                clipRule="evenodd"
-              >
-                <path d="M1.571 23.664l10.531-10.501 3.712 3.701-12.519 6.941c-.476.264-1.059.26-1.532-.011l-.192-.13zm9.469-11.56l-10.04 10.011v-20.022l10.04 10.011zm6.274-4.137l4.905 2.719c.482.268.781.77.781 1.314s-.299 1.046-.781 1.314l-5.039 2.793-4.015-4.003 4.149-4.137zm-15.854-7.534c.09-.087.191-.163.303-.227.473-.271 1.056-.275 1.532-.011l12.653 7.015-3.846 3.835-10.642-10.612z" />
-              </svg>
-              &nbsp; Download now
-            </Link>
-          </div>
-        </div>
-
-        <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-          <img
-            className="w-96"
-            src="https://i.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=1280&format=png&auto=webp&s=7177756d1f393b6e093596d06e1ba539f723264b"
-            alt="image1"
-          />
-        </div>
-      </aside>
-
-      <div className="grid  place-items-center sm:mt-20">
+    <div className="bg-gray-500 mt-25 mb-1 flex">
+      <div className="p-5 flex-2 flex flex-col mb-45 justify-center text-center">
+        <h1 className="text-5xl m-3 animate-typing font-medium">
+          HELLO, I'M GAURAV 👋
+        </h1>
+        <h2 className="text-xl m-3 font-semibold">
+          Website developer | Javascript & MERN Stack
+        </h2>
+      </div>
+      <div className="p-5 flex-1 bg-amber-200 text-center">
         <img
-          className="sm:w-96 w-48"
-          src="https://cdn-images-1.medium.com/max/773/1*t_G1kZwKv0p2arQCgYG7IQ.gif"
-          alt="image2"
+          src="me.jpeg"
+          alt="My image"
+          className="w-100 rounded-3xl mx-auto flex object-center"
         />
       </div>
-
-      <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">
-        Lorem Ipsum Yojo
-      </h1>
     </div>
   );
 }
