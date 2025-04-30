b94be952f9dabc851387e9005b0e75d27839ca9a
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-04-30 19:10:27 +0530

    Technical skill part of home page

---
+++ portfolio/portfolioFrontend/src/components/home/techSkills/TechSkills.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TechSkills() {
+  return (
+    <div>
+      <hr className="text-white h-0.5 my-8 bg-gray-200 border-0 " />
+      <h1>HELLO</h1>
+    </div>
+  );
+}
+
+export default TechSkills;
