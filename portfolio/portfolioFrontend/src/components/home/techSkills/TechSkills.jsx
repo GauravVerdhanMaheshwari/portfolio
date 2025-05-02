280440ac6c2a4341d888e11b2af706a0c960517f
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-02 19:07:27 +0530

    make new technical skill section

--- portfolio/portfolioFrontend/src/components/home/techSkills/TechSkills.jsx
+++ portfolio/portfolioFrontend/src/components/home/techSkills/TechSkills.jsx
@@ -1,30 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  ProgrammingLanguage,
+  LibraryFramework,
+  Tools,
+  Language,
+} from "./index";
 
 function TechSkills() {
+  const getItemClass = (item) =>
+    `text-xl cursor-pointer transition-all duration-200 ${
+      activeItem === item
+        ? "text-amber-400"
+        : "text-white hover:text-amber-400 hover:drop-shadow-[0_0_5px_rgba(255,191,0,0.6)]"
+    }`;
+
+  const [activeItem, setActiveItem] = useState("Programming Languages");
+
+  const items = [
+    "Programming Languages",
+    "Library & Framework",
+    "Tools",
+    "Language",
+  ];
+
+  const itemsDescription = {
+    "Programming Languages": <ProgrammingLanguage />,
+    "Library & Framework": <LibraryFramework />,
+    Tools: <Tools />,
+    Language: <Language />,
+  };
+
   return (
-    <>
-      <hr className="text-white h-0.5 my-8 bg-gray-200 border-0 " />
-      <h1 className="text-white mx-50 text-4xl">Technical Skills</h1>
-      <div className="my-10">
-        <div className="flex justify-between text-white mx-55">
-          <h1 className="text-xl font-mono">
-            {"<"}Programming Language{">"}
-          </h1>
-          <img
-            src="JS.webp"
-            alt="Javascript"
-            className="h-15 w-15 shadow-2xl shadow-amber-200"
-          />
-          <img src="java.webp" alt="Java" className="h-15 w-15" />
-          <img src="C.webp" alt="C" className="h-15 w-14" />
-          <img src="PHP.webp" alt="PHP" className="h-15 w-20" />
-          <img src="python.webp" alt="Python" className="h-15 w-15" />
-          <h1 className="text-xl mt-10 font-mono">
-            {"</"}Programming Language{">"}
-          </h1>
+    <div className="my-10">
+      <hr className="text-white h-0.5 my-8 bg-gray-200 border-0" />
+      <h1 className="text-white text-4xl font-semibold text-left mx-50 underline underline-offset-10 text-shadow-gray-200/65 text-shadow-[1px_1px_19px]">
+        Technical Skills
+      </h1>
+      <div className="flex flex-3 justify-center my-10 mx-50">
+        <div className="flex flex-1 justify-start">
+          <ul className="grid gap-6">
+            {items.map((item) => (
+              <li key={item}>
+                <h1
+                  className={getItemClass(item)}
+                  onClick={() => setActiveItem(item)}
+                >
+                  {item}
+                </h1>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex flex-1 ml-5 p-5 justify-start h-full w-full backdrop-blur-3xl rounded-xl bg-purple-400/90 shadow-[0px_0px_1000px] shadow-purple-400/90 border border-purple-100/80 inset-shadow-[0px_10px_100px] inset-shadow-purple-600">
+          {itemsDescription[activeItem]}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
