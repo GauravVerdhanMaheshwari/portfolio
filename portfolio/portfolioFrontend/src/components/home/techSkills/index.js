7620a51677c6a937179826fd19b8a8d3ed12785e
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-02 19:07:52 +0530

    common file to add element to the main file

---
+++ portfolio/portfolioFrontend/src/components/home/techSkills/index.js
@@ -0,0 +1,6 @@
+import LibraryFramework from "./LibraryFramework";
+import ProgrammingLanguage from "./ProgrammingLanguage";
+import Tools from "./Tools";
+import Language from "./Language";
+
+export { ProgrammingLanguage, LibraryFramework, Tools, Language };
