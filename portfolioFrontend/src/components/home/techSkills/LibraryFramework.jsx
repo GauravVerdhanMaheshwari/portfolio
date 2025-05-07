bfa4321324398ba54aa58fe6461cb3373eddf70d
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-07 23:42:51 +0530

    Add new skill images and update tech skills components to include Tailwind CSS, HTML, CSS, and Typescript

--- portfolioFrontend/src/components/home/techSkills/LibraryFramework.jsx
+++ portfolioFrontend/src/components/home/techSkills/LibraryFramework.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 function LibraryFramework() {
-  const libraryFramework = ["Node JS", "React"];
+  const libraryFramework = ["Node JS", "React", "Tailwind CSS"];
 
   const libraryFrameworkImage = {
     "Node JS": { image: "/nodeJS.webp", classname: "w-18 h-20 mt-5 mx-5" },
     React: { image: "/react.webp", classname: "w-22 h-20 mt-5 mx-5" },
+    "Tailwind CSS": {
+      image: "/tailwind.webp",
+      classname: "w-22 h-16 mt-5 mb-3 mx-5",
+    },
   };
 
   return (
