fd3c236086f5c935a82f810bc1200d6a776b394b
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-08 15:29:20 +0530

    Update About component: redesign layout with new styling, add personal introduction, and replace placeholder image with personal image for enhanced user engagement.

--- portfolioFrontend/src/components/about/About.jsx
+++ portfolioFrontend/src/components/about/About.jsx
@@ -1,30 +1,34 @@
 export default function About() {
   return (
-    <div className="py-16 bg-white">
-      <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-        <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-          <div className="md:5/12 lg:w-5/12">
-            <img
-              src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
-              alt="image"
-            />
-          </div>
-          <div className="md:7/12 lg:w-6/12">
-            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-              React development is carried out by passionate developers
-            </h2>
-            <p className="mt-6 text-gray-600">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
-              omnis voluptatem accusantium nemo perspiciatis delectus atque
-              autem! Voluptatum tenetur beatae unde aperiam, repellat expedita
-              consequatur! Officiis id consequatur atque doloremque!
-            </p>
-            <p className="mt-4 text-gray-600">
-              Nobis minus voluptatibus pariatur dignissimos libero quaerat iure
-              expedita at? Asperiores nemo possimus nesciunt dicta veniam
-              aspernatur quam mollitia.
-            </p>
-          </div>
+    <div className="px-4 py-16 sm:py-24 md:flex-row gap-8">
+      <h1 className="text-white text-2xl sm:text-3xl md:text-4xl font-semibold text-left m-5 sm:mx-8 md:mx-40 text-shadow-gray-200/65 text-shadow-[0px_0px_30px]">
+        About Me
+      </h1>
+      <hr className="text-white " />
+      <div className="flex flex-row justify-start mx-40">
+        <div className="flex-1">
+          <img
+            src="me.webp"
+            alt="About Me"
+            className="rounded-lg h-100 mt-10"
+          />
+        </div>
+        <div className="flex-1 text-white text-xl align-center justify-center mx-10 text-pretty my-auto">
+          <p>
+            Hello everyone, I'm <span className="text-blue-500">Gaurav Verdahan Maheshwari </span>
+          </p>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem error quibusdam debitis repellendus enim corrupti
+            quae inventore distinctio eius iure.
+          </p>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem error quibusdam debitis repellendus enim corrupti
+            quae inventore distinctio eius iure.
+          </p>
         </div>
       </div>
     </div>
